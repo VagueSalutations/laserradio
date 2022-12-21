d4a3acdac063e6f9bdd95e552a0d212cfdba0cc5
braden <VagueSalutations@users.noreply.github.com>
2022-12-21 13:07:26 -0500

    Pushed final files

--- encoder.py
+++ encoder.py
@@ -1,8 +1,25 @@
 
+from machine import Pin
 import time
+
+testdata = "1010101010101010101010101010101010101010101010101010101010101"
+testbaud = 1
 
 data = "Hello World!"
 baud = 9600
+
+
+# Initialize onboard LED pins
+led1 = Pin(8, Pin.OUT)
+led2 = Pin(9, Pin.OUT)
+led3 = Pin(10, Pin.OUT)
+led4 = Pin(11, Pin.OUT)
+led5 = Pin(12, Pin.OUT)
+leds = [led1, led2, led3, led4, led5]
+# Initialize laser pin
+Tx = Pin(1, Pin.OUT)
+Tx_enable = Pin(2, Pin.OUT)
+
 
 # Convert string into list of bytes
 bytes = []
@@ -15,10 +32,35 @@ for chr in data:
 bytestring = ''.join(bytes)
 
 """
-for bit in bytestring:
-    if bit == '1':
-        # Hold GPIO high
-    else:
-        # Hold GPIO low
-    time.sleep(1.0/baud)
+Tx_enable.off()
+while True:
+    for led in leds:
+        led.toggle()
+    Tx.toggle()
+    time.sleep(1)
 """
+
+Tx_enable.on()
+
+for idx, bit in enumerate(testdata):
+    
+    # Extract data and update the pin dipslay
+    if idx < len(testdata)-5:
+        i = idx+5
+        for led in leds:
+            if testdata[i] == '1':
+                led.on()
+            else:
+                led.off()
+            i -= 1
+
+    # Command laser
+    if bit == '1':
+        Tx.on()
+    else:
+        Tx.off()
+    
+    # Delay by baud rate
+    time.sleep(1.0/testbaud)
+
+
